9c0c04f64011b8496d51f660b976c5ef3b5b0ec2
Konstantin Gainbikhner <119727744+kegone@users.noreply.github.com>
2023-07-08 17:14:39 +0500

    Hide ALLOWED_HOSTS, change requiremensts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -4,11 +4,11 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'TOKEN')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '158.160.5.130', 'newkittygram.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1 localhost').split()
 
 INSTALLED_APPS = [
     'django.contrib.admin',
